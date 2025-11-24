c7704abf597b13a23ba17990796e9836e685ec1a
Erik Jun <erikjunyamazaki@gmail.com>
2025-11-24 15:26:06 -0300

    Modificações da interfaçe dos cards e primeiro card Canva

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   try {
     const response = await fetch("cards.json");
     const cards = await response.json();
-    const main = document.querySelector("main");
+    const main = document.querySelector("#cards-container");
 
     cards.forEach(card => {
       const div = document.createElement("div");
       div.classList.add("card");
       div.style.backgroundImage = `url(${card.image})`;
-      div.innerHTML = `
-        <div class="overlay">
-          <h2>${card.title}</h2>
-          <p>${card.text}</p>
-        </div>
-      `;
+      div.innerHTML = "";
       div.addEventListener("click", () => window.open(card.link, "_blank"));
       main.appendChild(div);
     });
