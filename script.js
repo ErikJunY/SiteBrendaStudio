163d68e5654c2e47ed4944064e91c65bae5f6442
Erik Jun <erikjunyamazaki@gmail.com>
2025-11-29 21:46:48 -0300

    Finalização de detalhes e cards oficiais adicionados

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ document.addEventListener("DOMContentLoaded", async () => {
       const div = document.createElement("div");
       div.classList.add("card");
       div.style.backgroundImage = `url(${card.image})`;
-      div.innerHTML = "";
-      div.addEventListener("click", () => window.open(card.link, "_blank"));
+
+      // Só adiciona o clique se tiver link válido
+      if (card.link && card.link !== "#" && card.link !== "") {
+        div.addEventListener("click", () => window.open(card.link, "_blank"));
+      }
+
       main.appendChild(div);
     });
   } catch (err) {
     console.error("Erro ao carregar os cards:", err);
   }
 });
+
